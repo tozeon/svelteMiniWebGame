8e5e77ba76f8eeab9bb9303fa19fa27ac7d41b10
Tim <tulxoro@hotmail.com>
2026-05-06 17:24:45 -0700

    feat: added word and dynamic active letter

---
+++ src/lib/components/letter.svelte
@@ -0,0 +1,32 @@
+<script lang="ts">
+	let { letter = $bindable(), activeLetter = $bindable(), index, wordSize } = $props();
+
+  const active = $derived(activeLetter === index);
+
+</script>
+
+{#if active}
+	<!-- slot with "underlines" -->
+	<div class="rounded-lg border-2 border-black bg-white p-4 text-center">
+		<!-- actual letters -->
+		<span class="m-2 border-b-2 border-black p-1 text-lg">
+			{#if letter != '_'}
+				{letter}
+			{:else}
+				&nbsp;
+			{/if}
+		</span>
+	</div>
+{:else}
+	<!-- slot with "underlines" -->
+	<div class="rounded-lg border border-gray-300 bg-white p-4 text-center" onclick={() => {activeLetter = index;}}>
+		<!-- actual letters -->
+		<span class="m-2 border-b-2 border-black p-1 text-lg">
+			{#if letter != '_'}
+				{letter}
+			{:else}
+				&nbsp;
+			{/if}
+		</span>
+	</div>
+{/if}
